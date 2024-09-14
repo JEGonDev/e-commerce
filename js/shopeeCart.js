0ba7f568938eb3cf5d5f4206972395475d03d9fb
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2024-09-14 09:50:09 -0500

    Primer avance carrito de compras

---
+++ js/shopeeCart.js
@@ -0,0 +1,6 @@
+const containerSelectedItems = document.getElementById('container-selecter-items')
+const containerPaySelectedItems = document.getElementById('pay-selected-items')
+
+const generateSelectedItems = (items) => {
+    
+}
